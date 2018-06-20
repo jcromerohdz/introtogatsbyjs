732b10a7c3b250728522d121e882a5a469b1ce9f
Christian <jcromerohdz@gmail.com>
2018-06-20 07:09:49 -0700

    Styled components added

--- frontBay-blog/src/components/header.js
+++ frontBay-blog/src/components/header.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components';
 
 import logo from '../images/frontbayLogo.png';
 
 
+const HeaderWrapper = styled.div`
+  background: #673ab7;
+  margin-bottom: 1.45rem;
+  h1{
+    img{
+      height: 100px;
+    }
+  }
+`;
+
+const HeaderBox = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderBox>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -31,18 +36,19 @@ const Header = ({ siteTitle }) => (
           <img src={logo} alt="Front Bay Logo" />
         </Link>
       </h1>
-    </div>
     <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-    </nav>
-  </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>  
+  </HeaderBox>
+   
+  </HeaderWrapper>
 )
 
 export default Header
