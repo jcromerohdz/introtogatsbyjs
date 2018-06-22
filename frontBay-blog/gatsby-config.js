e61c18a5addec2e8f882ed772823d9640013fa54
Christian <jcromerohdz@gmail.com>
2018-06-22 12:04:50 -0700

    Data Layer with GraphQL

--- frontBay-blog/gatsby-config.js
+++ frontBay-blog/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
+    desc: 'FrontBay new blog',
   },
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
--- frontBay-blog/src/pages/index.js
+++ frontBay-blog/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
     <h1>Hi people</h1>
+    <p>{data.site.siteMetadata.title}</p>
+    <p>{data.site.siteMetadata.desc}</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
@@ -11,3 +13,14 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query SiteMeta {
+    site{
+      siteMetadata{
+        title
+        desc
+      }
+    }
+  }
+`
